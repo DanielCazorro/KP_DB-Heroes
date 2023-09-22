97db34cf7e856caec296e71181faafd8833fe0d4
Daniel Cazorro <leinadhunter10@hotmail.com>
2023-09-22 19:08:00 +0200

    Prepare for network

---
+++ KP-DBHeroes/KP-DBHeroes/Hero List/HeroListViewController.swift
@@ -0,0 +1,29 @@
+//
+//  HeroListViewController.swift
+//  KP-DBHeroes
+//
+//  Created by Daniel Cazorro Frías on 22/9/23.
+//
+
+import UIKit
+
+class HeroListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
