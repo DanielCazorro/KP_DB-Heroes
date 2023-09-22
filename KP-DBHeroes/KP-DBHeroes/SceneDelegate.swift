6f0162cbac7a5899c16221252df61e5dfd6637b1
Daniel Cazorro <leinadhunter10@hotmail.com>
2023-09-22 11:05:40 +0200

    V0.1 Login Screen + Assets

---
+++ KP-DBHeroes/KP-DBHeroes/SceneDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  SceneDelegate.swift
+//  KP-DBHeroes
+//
+//  Created by Daniel Cazorro Frías on 22/9/23.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let scene = (scene as? UIWindowScene) else {
+            return
+        }
+        let window = UIWindow(windowScene: scene)
+        let viewController = LoginViewController()
+        window.rootViewController = viewController
+        window.makeKeyAndVisible()
+        self.window = window
+    }
+}
+
---
+++ KP-DBHeroes/KP-DBHeroes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KP-DBHeroes
+//
+//  Created by Daniel Cazorro Frías on 22/9/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
