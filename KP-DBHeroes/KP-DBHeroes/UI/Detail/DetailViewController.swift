c9f707b86a4056fb25c641a4c8e98b9727c963a8
Daniel Cazorro Frías <leinadhunter10@hotmail.com>
2024-02-03 15:51:32 +0100

    Fix weak self detailview

--- KP-DBHeroes/KP-DBHeroes/UI/Detail/DetailViewController.swift
+++ KP-DBHeroes/KP-DBHeroes/UI/Detail/DetailViewController.swift
@@ -1,5 +1,5 @@
 //
-//  DragonBallHeroesViewController.swift
+//  DetailViewController.swift
 //  KP-DBHeroes
 //
 //  Created by Daniel Cazorro Frías on 30/9/23.
@@ -9,17 +9,22 @@ import UIKit
 
 class DetailViewController: UIViewController {
     
-    private let hCharacter: CharacterProtocol
-    private let hImage: UIImage?
-    private var hTransformations: [Transformations] = []
+    // MARK: - Properties
     
-// MARK: - OUTLET -
+    private let hCharacter: CharacterProtocol // El personaje que se mostrará en detalle
+    private let hImage: UIImage? // La imagen del personaje, puede ser nula
+    private var hTransformations: [Transformations] = [] // Las transformaciones del personaje
     
-    @IBOutlet private weak var hImageView: UIImageView!
-    @IBOutlet private weak var hNameLabel: UILabel!
-    @IBOutlet private weak var hDescriptionLabel: UILabel!
-    @IBOutlet private weak var button: UIButton!
+    // MARK: - Outlets
     
+    @IBOutlet private weak var hImageView: UIImageView! // Vista de imagen para mostrar la imagen del personaje
+    @IBOutlet private weak var hNameLabel: UILabel! // Etiqueta para mostrar el nombre del personaje
+    @IBOutlet private weak var hDescriptionLabel: UILabel! // Etiqueta para mostrar la descripción del personaje
+    @IBOutlet private weak var button: UIButton! // Botón para mostrar las transformaciones del personaje
+    
+    // MARK: - Initialization
+    
+    // Inicializador personalizado que recibe el personaje y la imagen del personaje
     init(character: CharacterProtocol, image: UIImage?) {
         self.hCharacter = character
         self.hImage = image
@@ -30,47 +35,57 @@ class DetailViewController: UIViewController {
         fatalError("init(coder:\(coder)) has not been implemented")
     }
     
+    // MARK: - Lifecycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        title = hCharacter.title
-        hDescriptionLabel.numberOfLines = .zero
-        configureButton()
-        updateUI()
-        let client = NetworkModel()
-        client.fetchTransformations(requestData: TransformationRequest(id: hCharacter.id)) { result in
+        title = hCharacter.title // Configura el título de la vista con el nombre del personaje
+        hDescriptionLabel.numberOfLines = 0 // Configura el número de líneas de la etiqueta de descripción como cero para mostrar múltiples líneas si es necesario
+        configureButton() // Configura el botón
+        updateUI() // Actualiza la interfaz de usuario con la información del personaje
+        let client = NetworkModel() // Instancia del cliente de red
+        client.fetchTransformations(requestData: TransformationRequest(id: hCharacter.id)) { [weak self] result in
             switch result {
-            case .success(let transformations):
-                self.button.isHidden = false
-                self.hTransformations = transformations.map { transformation in
+            case .success(let transformations): // Si la solicitud de transformaciones es exitosa
+                self?.button.isHidden = false // Muestra el botón
+                // Mapea las transformaciones recibidas a objetos Transformations y las asigna a la propiedad hTransformations
+                self?.hTransformations = transformations.map { transformation in
                     Transformations(url: URL(string: transformation.photo), title: transformation.name, description: transformation.description)
                 }
-                self.button.isHidden = self.hTransformations.count == .zero
-            case .failure(let error):
-                print("Error fetching transformations: \(error)")
+                // Oculta el botón si no hay transformaciones disponibles
+                self?.button.isHidden = self?.hTransformations.count == 0
+            case .failure(let error): // Si hay un error al obtener las transformaciones
+                print("Error fetching transformations: \(error)") // Imprime el error
             }
         }
     }
     
+    // MARK: - Actions
+    
+    // Acción cuando se selecciona el botón de transformaciones
     @IBAction func didSelectButton(_ sender: UIButton) {
-       
+        // Crea una instancia del controlador de vista TransformationsViewController con las transformaciones del personaje y lo presenta
         let transformationsVC = TransformationsViewController(transformations: self.hTransformations)
-            self.navigationController?.pushViewController(transformationsVC, animated: true)
-        
+        self.navigationController?.pushViewController(transformationsVC, animated: true)
     }
 }
 
+// MARK: - Private Methods
+
 private extension DetailViewController {
     
+    // Método para actualizar la interfaz de usuario con la información del personaje
     func updateUI() {
-        self.hImageView.image = hImage
-        self.hNameLabel.text = hCharacter.title
-        self.hDescriptionLabel.text = hCharacter.description
+        self.hImageView.image = hImage // Establece la imagen del personaje en la vista de imagen
+        self.hNameLabel.text = hCharacter.title // Establece el nombre del personaje en la etiqueta de nombre
+        self.hDescriptionLabel.text = hCharacter.description // Establece la descripción del personaje en la etiqueta de descripción
     }
     
+    // Método para configurar el botón de transformaciones
     func configureButton() {
-        button.isHidden = true
-        button.backgroundColor = .systemBlue
-        button.setTitle("Transformaciones", for: .normal)
-        button.setTitleColor(.white, for: .normal)
+        button.isHidden = true // Oculta el botón por defecto
+        button.backgroundColor = .systemBlue // Establece el color de fondo del botón como azul
+        button.setTitle("Transformaciones", for: .normal) // Establece el título del botón como "Transformaciones"
+        button.setTitleColor(.white, for: .normal) // Establece el color del título del botón como blanco
     }
 }
--- KP-DBHeroes/KP-DBHeroes/UI/Hero List/HeroesViewController.swift
+++ KP-DBHeroes/KP-DBHeroes/UI/Hero List/HeroesViewController.swift
@@ -9,48 +9,63 @@ import UIKit
 
 class HeroesViewController: UIViewController {
 
-// MARK: - OUTLET -
-    
+    // MARK: - Properties
     var characters: [CharacterProtocol] = []
-    
+
+    // MARK: - Outlets
     @IBOutlet weak var tableView: UITableView!
 
+    // MARK: - Lifecycle Methods
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "Heroes"
+        
+        // Register custom cell
         tableView.register(UINib(nibName: "HeroesCell", bundle: nil), forCellReuseIdentifier: "HeroCell")
     }
 }
 
+// MARK: - UITableViewDataSource
+
 extension HeroesViewController: UITableViewDataSource {
     
     func numberOfSections(in tableView: UITableView) -> Int {
-        1
+        return 1
     }
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        characters.count
+        return characters.count
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let character = characters[indexPath.row]
-        guard let cell = tableView.dequeueReusableCell(withIdentifier: "HeroCell") as? HeroesCell else { return UITableViewCell() }
+        
+        // Dequeue reusable cell
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "HeroCell") as? HeroesCell else {
+            return UITableViewCell()
+        }
+        
+        // Configure cell
         cell.configure(character: character)
         cell.accessoryType = .disclosureIndicator
         return cell
     }
-
 }
+
+// MARK: - UITableViewDelegate
 
 extension HeroesViewController: UITableViewDelegate {
 
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         let character = characters[indexPath.row]
         var image: UIImage?
+        
+        // Get the image from the selected cell
         if let cell = tableView.cellForRow(at: indexPath) as? HeroesCell {
             image = cell.heroImageView.image
         }
 
+        // Instantiate detail view controller and push it to navigation stack
         let heroDetail = DetailViewController(character: character, image: image)
         navigationController?.pushViewController(heroDetail, animated: true)
     }
