c5549c260e26cdc857f1253552d04cc9b26fa935
Daniel Cazorro Frías <leinadhunter10@hotmail.com>
2024-02-03 20:33:07 +0100

    Again not working

--- KP-DBHeroes/KP-DBHeroes/Views/Login/LoginViewController.swift
+++ KP-DBHeroes/KP-DBHeroes/Views/Login/LoginViewController.swift
@@ -9,34 +9,35 @@ import UIKit
 
 class LoginViewController: UIViewController {
     
-    // Properties
     let client = NetworkModel()
 
-    // MARK: - OUTLET -
+// MARK: - OUTLET -
+    
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var passwordTextfield: UITextField!
     @IBOutlet weak var loginButton: UIButton!
 
-    // MARK: - ACTION -
+// MARK: - ACTION -
+    
     @IBAction func didTapLogin(_ sender: UIButton) {
         guard let email = emailTextField.text, let password = passwordTextfield.text else { return }
         
         
-        client.login(requestData: LoginRequest(username: email, password: password)) { [weak self] result in
+        client.login(requestData: LoginRequest(username: email, password: password)) { result in
             switch result {
-            case .success(_):
+            case .success(let token):
                 
                 let hero = DragonBallHeroesViewController(nibName: "HeroesViewController", bundle: nil)
                 let navController = UINavigationController(rootViewController: hero)
                 
-                self?.view.window?.rootViewController = navController
+                self.view.window?.rootViewController = navController
                 
-            case .failure(_):
+            case .failure(let error):
              
                 let popAlert = UIAlertController(title: "Error", message: "Inicio de sesión fallido. Por favor, inténtalo de nuevo.", preferredStyle: .alert)
                 popAlert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
                 
-                self?.present(popAlert, animated: true, completion: nil)
+                self.present(popAlert, animated: true, completion: nil)
             }
         }
     }
