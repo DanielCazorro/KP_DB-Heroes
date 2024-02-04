937ba775dcb174e881ce352506e79ef28ce7cc0a
Daniel Cazorro Frías <leinadhunter10@hotmail.com>
2024-02-04 12:23:16 +0100

    Login working: Update push method and add WEAK SELF

--- KP-DBHeroes/KP-DBHeroes/Views/Login/LoginViewController.swift
+++ KP-DBHeroes/KP-DBHeroes/Views/Login/LoginViewController.swift
@@ -7,37 +7,36 @@
 
 import UIKit
 
+/// Clase que maneja la vista de inicio de sesión
 class LoginViewController: UIViewController {
     
     // MARK: - IBOUTLET -
-    @IBOutlet weak var emailTextField: UITextField!
-    @IBOutlet weak var passwordTextfield: UITextField!
-    @IBOutlet weak var loginButton: UIButton!
-    
-    
-    // MARK: - Lifecycle
-    override func viewDidLoad() {
-        super.viewDidLoad()
-    }
+    @IBOutlet weak var emailTextField: UITextField! // Campo de texto para introducir el email
+    @IBOutlet weak var passwordTextfield: UITextField! // Campo de texto para introducir la contraseña
+    @IBOutlet weak var loginButton: UIButton! // Botón para iniciar sesión
     
     // MARK: - ACTION -
-    
+    /// Método llamado cuando se toca el botón de inicio de sesión
     @IBAction func didTapLogin(_ sender: UIButton) {
-        // Obtenemos el email y la contraseña de los textfields, si están disponibles
+        // Obtenemos el email y la contraseña de los campos de texto, asegurándonos de que no sean nulos
         let email = emailTextField.text ?? ""
         let password = passwordTextfield.text ?? ""
         
-        // Creamos una instancia del modelo de red para realizar la solicitud de inicio de sesión
-        let model = NetworkModel()
+        // Creamos una instancia del modelo de red para manejar las solicitudes a la API
+        let client = NetworkModel()
         
-        // Realizamos la solicitud de inicio de sesión al servidor
-        model.login(user: email, password: password) { [weak self] result in
+        // Realizamos la solicitud de inicio de sesión al servidor utilizando el modelo de red
+        client.login(user: email, password: password) { [weak self] result in
+            // Comprobamos si la solicitud de inicio de sesión fue exitosa
             guard case let .success(token) = result else {
-                return
+                return // Si no fue exitosa, no hacemos nada más
             }
             
-            DispatchQueue.main.async{
+            // Si la solicitud de inicio de sesión fue exitosa y se obtuvo un token no vacío,
+            // realizamos cambios en la interfaz de usuario en el hilo principal
+            DispatchQueue.main.async {
                 if !token.isEmpty {
+                    // Creamos una instancia del controlador de vista de héroes y lo establecemos como la vista actual
                     let heroesViewController = HeroesViewController()
                     self?.navigationController?.setViewControllers([heroesViewController], animated: true)
                 }
