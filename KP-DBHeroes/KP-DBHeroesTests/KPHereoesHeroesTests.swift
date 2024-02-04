f737369eec17cb4eccfb7c8f483ed8f0724ed82e
Daniel Cazorro Frías <leinadhunter10@hotmail.com>
2024-02-04 15:12:21 +0100

    Test done

--- KP-DBHeroes/KP-DBHeroesTests/KPHereoesHeroesTests.swift
+++ KP-DBHeroes/KP-DBHeroesTests/KPHereoesHeroesTests.swift
@@ -9,36 +9,138 @@ import XCTest
 @testable import KP_DBHeroes
 
 final class KPHereoesTests: XCTestCase {
-
-    override func setUpWithError() throws { }
-
-    override func tearDownWithError() throws { }
-
-    func testExample() throws {
-        let hero = Hero.init(id: "Daniel", url: nil, title: "KP", description: "Master")
-        XCTAssertEqual(hero.id, "Daniel")
+    private let goodURL = URL(string: "goodURL")
+    private let badURL = URL(string: "badURL")
+    
+    func testHeroInitialization() {
+        guard let goodURL = URL(string: "https://example.com/goku.jpg") else {
+                XCTFail("Failed to create URL.")
+                return
+            }
+        
+        let hero = Hero(photo: goodURL, id: "1", favorite: true, name: "Goku", description: "Saiyan warrior")
+        
+        XCTAssertEqual(hero.id, "1")
+        XCTAssertEqual(hero.name, "Goku")
+        XCTAssertEqual(hero.description, "Saiyan warrior")
+        XCTAssertEqual(hero.photo, goodURL)
     }
     
-    func testDragonBallHeroInitialization() {
-        
-            let heroInformation = """
-            {
-                "id": "5",
-                "name": "Vegeta",
-                "description": "Saiyan prince",
-                "photo": "https://example.com/vegeta.jpg"
-            }
-            """.data(using: .utf8)!
-
-            let decoder = JSONDecoder()
-            do {
-                let hero = try decoder.decode(DBHeroResponse.self, from: heroInformation)
-                XCTAssertEqual(hero.id, "5")
-                XCTAssertEqual(hero.name, "Vegeta")
-                XCTAssertEqual(hero.description, "Saiyan prince")
-                XCTAssertEqual(hero.photo, "https://example.com/vegeta.jpg")
-            }
-        catch { XCTFail("Decoding failed with error: \(error)") }
-        
+    func testTransformationInitialization() {
+        // URL válida para la foto de la transformación
+        guard let transformationURL = URL(string: "https://example.com/super-saiyan.jpg") else {
+            XCTFail("Failed to create URL.")
+            return
         }
+        
+        // Inicialización de una transformación con valores válidos
+        let transformation = Transformation(photo: transformationURL, id: "1", name: "Super Saiyan", description: "Powerful transformation")
+        
+        // Verificación de los valores de inicialización
+        XCTAssertEqual(transformation.id, "1")
+        XCTAssertEqual(transformation.name, "Super Saiyan")
+        XCTAssertEqual(transformation.description, "Powerful transformation")
+        XCTAssertEqual(transformation.photo, transformationURL)
+    }
+}
+
+//MARK: -  Local DATA Model Test
+final class LocalDataModelTests: XCTestCase {
+    static let testToken = "TheForceToken"
+
+    override func tearDownWithError() throws {
+        LocalDataModel.deleteToken()
+    }
+    
+    func testSaveToken() throws {
+        LocalDataModel.save(token: LocalDataModelTests.testToken)
+        let retrievedToken = LocalDataModel.getToken()
+        XCTAssertEqual(retrievedToken, LocalDataModelTests.testToken, "Retrieved token should be equal to test one")
+    }
+    
+    func testDeleteToken() throws {
+        LocalDataModel.save(token: LocalDataModelTests.testToken)
+        LocalDataModel.deleteToken()
+        let retrievedToken = LocalDataModel.getToken()
+        XCTAssertNil(retrievedToken, "There should be no token retrieved")
+    }
+    
+}
+
+//MARK: - ViewCell
+class HeroesCellTests: XCTestCase {
+    
+    func testConfigure() {
+        // Prepare test data
+        let character = TableViewCellRepresentableMock()
+        
+        // Prepare cell
+        let cell = HeroesCell()
+        let heroNameLabel = UILabel()
+        let heroDescLabel = UILabel()
+        let heroImageView = UIImageView()
+        cell.heroNameLabel = heroNameLabel
+        cell.heroDescLabel = heroDescLabel
+        cell.heroImageView = heroImageView
+        
+        // Call configure method
+        cell.configure(character: character)
+        
+        // Assertions
+        XCTAssertEqual(cell.heroNameLabel.text, character.name)
+        XCTAssertEqual(cell.heroDescLabel.text, character.description)
+
+    }
+}
+
+// Mock implementation of TableViewCellRepresentable for testing
+class TableViewCellRepresentableMock: TableViewCellRepresentable {
+    var photo: URL = URL(string: "https://example.com/image.jpg")!
+    var name: String = "Test Name"
+    var description: String = "Test Description"
+}
+
+//MARK: -Transforamtion View
+class TransformationViewControllerTests: XCTestCase {
+    
+    func testTableViewDataSourceMethods() {
+        let viewController = TransformationViewController()
+        
+        // Load the view to trigger viewDidLoad and register the cell
+        viewController.loadViewIfNeeded()
+        
+        guard let tableView = viewController.tvTransformations else {
+            XCTFail("TableView is nil")
+            return
+        }
+        // Verify that the tableView's data source is set to the view controller
+        XCTAssertTrue(tableView.dataSource === viewController)
+        
+        // Verify that the number of sections is correct
+        XCTAssertEqual(viewController.numberOfSections(in: tableView), 1)
+        
+        // Test numberOfRowsInSection method
+        XCTAssertEqual(viewController.tableView(tableView, numberOfRowsInSection: 0), 0)
+        
+    }
+    
+    func testTableViewDelegateMethods() {
+        let viewController = TransformationViewController()
+        
+        // Load the view to trigger viewDidLoad and register the cell
+        viewController.loadViewIfNeeded()
+        
+        guard let tableView = viewController.tvTransformations else {
+            XCTFail("TableView is nil")
+            return
+        }
+        
+        // Verify that the tableView's delegate is set to the view controller
+        XCTAssertTrue(tableView.delegate === viewController)
+        
+        // Test heightForRowAt method
+        let indexPath = IndexPath(row: 0, section: 0)
+        let height = viewController.tableView(tableView, heightForRowAt: indexPath)
+        XCTAssertEqual(height, UITableView.automaticDimension)
+    }
 }
--- KP-DBHeroes/KP-DBHeroesTests/NetworkModelTest.swift
+++ KP-DBHeroes/KP-DBHeroesTests/NetworkModelTest.swift
@@ -8,18 +8,19 @@
 import XCTest
 @testable import KP_DBHeroes
 
-// AL MODIFICAR EL ARCHIVO NETWORK MODEL PARA LA NUEVA VERSIÓN DE LA APP, ESTE TEST YA NO FUNCIONA, POR LO QUE EN PRINCIPIO LO DEJO COMENTADO PUESTO QUE EL DE ARRIBA EJECUTA BIEN
-
-/*
 final class NetworkModelTests: XCTestCase {
     private var sut: NetworkModel!
+    private var expectToken = "The Force Be With You"
+    private var client: APIClientProtocol!
     
     override func setUp() {
         super.setUp()
         let configuration = URLSessionConfiguration.ephemeral
         configuration.protocolClasses = [MockURLProtocol.self]
         let session = URLSession(configuration: configuration)
-        sut = NetworkModel(session: session)
+        LocalDataModel.save(token: expectToken)
+        client = APIClient(session: session)
+        sut = NetworkModel(client: client)
     }
     
     override func tearDown() {
@@ -27,26 +28,23 @@ final class NetworkModelTests: XCTestCase {
         sut = nil
     }
     
-    func testLogin() {
-        let expectedToken = "Some Token"
-        let someUser = "SomeUser"
-        let somePassword = "SomePassword"
+    func testLogin() throws {
+        let data = try XCTUnwrap(expectToken.data(using: .utf8))
+        let someUser = "daniel@hotmail.com"
+        let somePassword = "TheForce"
         
+        MockURLProtocol.error = nil
         MockURLProtocol.requestHandler = { request in
             let loginString = String(format: "%@:%@", someUser, somePassword)
-            let loginData = loginString.data(using: .utf8)!
+            let loginData = loginString.data(using: String.Encoding.utf8)!
             let base64LogingString = loginData.base64EncodedString()
             
             XCTAssertEqual(request.httpMethod, "POST")
-            XCTAssertEqual(
-                request.value(forHTTPHeaderField: "Authorization"),
-                "Basic \(base64LogingString)"
-            )
+            XCTAssertEqual(request.value(forHTTPHeaderField: "Authorization"),"Basic \(base64LogingString)")
             
-            let data = try XCTUnwrap(expectedToken.data(using: .utf8))
             let response = try XCTUnwrap(
                 HTTPURLResponse(
-                    url: URL(string: "https://dragonball.keepcoding.education")!,
+                    url: .init(string: "https://dragonball.keepcoding.education/")!,
                     statusCode: 200,
                     httpVersion: nil,
                     headerFields: ["Content-Type": "application/json"]
@@ -56,27 +54,75 @@ final class NetworkModelTests: XCTestCase {
         }
         
         let expectation = expectation(description: "Login success")
-        
-        sut.login(
-            user: someUser,
-            password: somePassword
-        ) { result in
+        var receivedToken: String?
+        sut.login(user: someUser, password: somePassword) { result in
             guard case let .success(token) = result else {
                 XCTFail("Expected success but received \(result)")
                 return
             }
-            
-            XCTAssertEqual(token, expectedToken)
+            expectation.fulfill()
+            receivedToken = token
+        }
+        
+        wait(for: [expectation], timeout: 1)
+        XCTAssertNotNil(receivedToken, "Token Should be received")
+        XCTAssertEqual(receivedToken, expectToken)
+    }
+    
+    func testLoginError() throws {
+        MockURLProtocol.error = .invalidURL
+        
+        let expectation = expectation(description: "Error de login")
+        sut.login(user: "", password: "") { result in
+            guard case .failure(.invalidURL) = result else {
+                XCTFail("We expect invalid URL but we take \(result)")
+                return
+            }
             expectation.fulfill()
         }
         
         wait(for: [expectation], timeout: 1)
     }
+    
+    func testGetHeroes() throws {
+        let expectedResponse = """
+        [{"favorite":false,"name":"Maestro Roshi","photo":"https://cdn.alfabetajuega.com/alfabetajuega/2020/06/Roshi.jpg?width=300","id":"14BB8E98-6586-4EA7-B4D7-35D6A63F5AA3","description":"Es un maestro de artes marciales que tiene una escuela, donde entrenará a Goku y Krilin para los Torneos de Artes Marciales. Aún en los primeros episodios había un toque de tradición y disciplina, muy bien representada por el maestro. Pero Muten Roshi es un anciano extremadamente pervertido con las chicas jóvenes, una actitud que se utilizaba en escenas divertidas en los años 80. En su faceta de experto en artes marciales, fue quien le enseñó a Goku técnicas como el Kame Hame Ha"}]
+        """
+        let data = try XCTUnwrap(expectedResponse.data(using: .utf8))
+        MockURLProtocol.error = nil
+        MockURLProtocol.requestHandler = { request in
+            XCTAssertEqual(request.httpMethod, "POST")
+            XCTAssertEqual(request.value(forHTTPHeaderField: "Authorization"), "Bearer \(self.expectToken)")
+            let response = try XCTUnwrap(
+                HTTPURLResponse(
+                    url: .init(string: "https://dragonball.keepcoding.education")!,
+                    statusCode: 200,
+                    httpVersion: nil,
+                    headerFields: ["Content-Type": "application/json"]
+                )
+            )
+            return (response, data)
+        }
+        let expectation = expectation(description: "Get heroes success")
+        var receivedHeroes = [Hero]()
+        sut.getHeroes { result in
+            guard case let .success(heroes) = result else {
+                XCTFail("Expected success but received \(result)")
+                return
+            }
+            receivedHeroes = heroes
+            expectation.fulfill()
+        }
+        
+        wait(for: [expectation], timeout: 1)
+        XCTAssertFalse(receivedHeroes.isEmpty)
+        XCTAssertEqual(receivedHeroes.first?.name, "Maestro Roshi")
+    }
 }
 
 // OHHTTPStubs
 final class MockURLProtocol: URLProtocol {
-    static var error: NetworkModel.NetworkError?
+    static var error: NetworkError?
     static var requestHandler: ((URLRequest) throws -> (HTTPURLResponse, Data))?
     
     override class func canInit(with request: URLRequest) -> Bool {
@@ -110,4 +156,4 @@ final class MockURLProtocol: URLProtocol {
     
     override func stopLoading() { }
  }
- */
+ 
